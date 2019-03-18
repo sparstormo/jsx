d5c6ff23044dcc0e92d48b387e55eda4e5b57262
Spar <spar@Spars-MacBook-Air.local>
2019-03-18 14:46:28 -0500

    first

--- src/index.js
+++ src/index.js
@@ -20,6 +20,6 @@ const App = () => {
     );
 }
 
-// Take the React component and show it on the screen
+// Take React component and show it on the screen
 
 ReactDOM.render(< App />, document.querySelector('#root'));
